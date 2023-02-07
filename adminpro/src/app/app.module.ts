be51f1645890d98b700a12913d8f423de961f78e
Juan Perez <35706696+jcperezz@users.noreply.github.com>
2023-02-07 18:46:23 +1300

    Create new empty components

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { LoginComponent } from './auth/login/login.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     LoginComponent,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    ProgressComponent,
+    Graficas1Component,
+    DashboardComponent,
+    RegisterComponent,
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule
---
+++ adminpro/src/app/pages/graficas1/graficas1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas1',
+  templateUrl: './graficas1.component.html',
+  styles: [
+  ]
+})
+export class Graficas1Component {
+
+}
