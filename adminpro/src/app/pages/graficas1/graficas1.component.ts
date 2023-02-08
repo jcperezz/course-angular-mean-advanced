709fb2c7a79752c4f4ee9128440f62fb52af273a
Juan Perez <35706696+jcperezz@users.noreply.github.com>
2023-02-08 22:05:06 +1300

    Add the graph component

--- adminpro/src/app/pages/graficas1/graficas1.component.ts
+++ adminpro/src/app/pages/graficas1/graficas1.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
+
+
 @Component({
   selector: 'app-graficas1',
   templateUrl: './graficas1.component.html',
@@ -7,5 +10,17 @@ import { Component } from '@angular/core';
   ]
 })
 export class Graficas1Component {
+
+    // Doughnut
+    public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+    public doughnutChartData: ChartData<'doughnut'> = {
+      labels: this.doughnutChartLabels,
+      datasets: [
+        { data: [ 350, 450, 100 ] },
+        { data: [ 50, 150, 120 ] },
+        { data: [ 250, 130, 70 ] }
+      ]
+    };
+    public doughnutChartType: ChartType = 'doughnut';
 
 }
--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { NgChartsModule } from 'ng2-charts';
 
 import { FormsModule } from "@angular/forms";
 import { ProgressComponent } from './progress/progress.component';
@@ -24,6 +25,7 @@ import { ComponentsModule } from '../components/components.module';
     FormsModule,
     RouterModule,
     ComponentsModule,
+    NgChartsModule,
   ]
 })
 export class PagesModule { }
