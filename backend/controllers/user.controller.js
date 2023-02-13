f73ee5b6a59d91f86cbe8a19b0b1de202652ec72
Juan Perez <35706696+jcperezz@users.noreply.github.com>
2023-02-13 15:38:41 +1300

    Add email validation

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
- const User = require('../models/user.model');
- 
- const getUsers = async (req, res) => {
+const { response } = require('express');
+const User = require('../models/user.model');
+
+const getUsers = async (req, res) => {
 
     const users = await User.find({}, 'name email role google');
 
@@ -11,19 +12,42 @@
 
 };
 
-const createUser = async (req, res) => {
-    
+const createUser = async (req, res = response) => {
+
     // 1. Read the body request
     const { email, password, nombre } = req.body;
 
-    const newUser = new User( req.body );
+    const existsEmail = await User.findOne({ email });
 
-    await newUser.save();
+    if(existsEmail) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'El correo ya está registrado',
+        });
+    }
 
-    res.json({
-        ok: true,
-        user: newUser,
-    });
+    try {
+
+        // 2. Parse the body request to User object
+        const newUser = new User(req.body);
+        // 3. Save on the BD
+        await newUser.save();
+        // 4. Build the response
+        res.json({
+            ok: true,
+            user: newUser,
+        });
+
+    } catch (error) {
+        // 5. Catch errors
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error no esperado'
+        });
+    }
+
+
 
 }
 
